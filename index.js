0b4b754a78bc23e47f4157b4bb6d863365adcf7d
Galib-23 <aagalib2323@gmail.com>
2023-11-17 18:23:00 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -3,16 +3,36 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-
 //middleware
 app.use(cors());
 app.use(express.json());
 
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ogz7mxs.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // await client.connect();
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res) => {
     res.send('munch magnet running');
 })
-
 app.listen(port , () => {
     console.log(`Munch Magnet Running On Port ${port}`);
 })
