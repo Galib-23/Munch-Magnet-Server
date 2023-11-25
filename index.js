26212356ef729eda51a13eefebedbf03c157d19e
Galib-23 <aagalib2323@gmail.com>
2023-11-25 21:02:11 +0600

    User Delete From Admin Panel Api Created

--- index.js
+++ index.js
@@ -41,6 +41,10 @@ async function run() {
       const result = await cartCollection.find(query).toArray();
       res.send(result);
     })
+    app.get('/users', async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
 
 
 
@@ -71,6 +75,12 @@ async function run() {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
       const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    })
+    app.delete('users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     })
 
