cc02945bf63ed4b7831795e9890568f53f3378f1
Galib-23 <aagalib2323@gmail.com>
2023-11-30 18:41:47 +0600

    Update API created

--- index.js
+++ index.js
@@ -152,6 +152,22 @@ async function run() {
       const result = await userCollection.updateOne(filter, updatedDoc);
       res.send(result);
     })
+    app.patch('/menu/:id', async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = { _id: id};
+      const updatedDoc = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          recipe: item.recipe,
+          image: item.image
+        }
+      }
+      const result = await menuCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
 
 
 
